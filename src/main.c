556ba32e24dbab527063159114c4b045b0480dab
ww <ww@www.ww>
2025-10-16 02:35:08 +0200

    Stylized pseudo-flux of program.

---
+++ src/main.c
@@ -0,0 +1,29 @@
+#include "../include/ft_ping.h"
+
+int main(int argc, char **argv) {
+
+    t_env env;
+
+    memset(&env, 0, sizeof(env));
+
+    //parse_args(argc, argv, &env.opts);
+
+    //resolve_target(env.opts.target_str, &env.target);
+
+    //env.sockfd = open_raw_socket();
+
+    //set_socket_timeout(env.sockfd, FT_PING_RECV_TIMEOUT_MS);
+
+    //env.ident = (uint16_t)getpid();
+
+    //stats_init(&env.stats);
+    //env.running = true;
+    //gettimeofday(&env.start_time, NULL);
+
+    //ping_run(&env);
+
+    //close(env.sockfd);
+
+    return 0;
+}
+
